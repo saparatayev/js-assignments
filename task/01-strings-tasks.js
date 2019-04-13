e95995a74943ebc4ab6a382d873d1c1ef3f7f071
Sapar Atayev <sopa.97@mail.ru>
2019-04-13 12:16:14 +0300

    feat: solve solve some string tasks

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -239,7 +239,8 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    if(typeof value == 'string' || value instanceof String) return true;
+    return false;
 }
 
 
